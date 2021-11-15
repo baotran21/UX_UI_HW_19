57716d7418a30f722ae7119f06b1f250e8f08468
baotran21 <btran8731@gmail.com>
2021-11-15 18:35:10 -0500

    hi

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-console.log("your index.js file is loaded correctly!")
-$('h1').animate({
-    height: '200px'
-}, 5000, 'swing');
+
+function myFunction() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+  }
+  
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
